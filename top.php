aca49fca6105992f279981a2faf911f853e5fd92
Folkmann <theme1256@gmail.com>
2016-01-28 21:47:07 +0100

    Fælles topfil, som tjekker for om der er logget ind

---
+++ top.php
@@ -0,0 +1,26 @@
+<?php
+	include "common.php";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Folkmann mailadmin</title>
+		<meta charset="utf-8">
+		<meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
+		<meta http-equiv="Pragma" content="no-cache"/>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<div class="header">
+			Folkmann mailadmin
+			<?php if(!empty($d)){?>
+			<span>Domæne: <?php echo $d;?></span>
+			<?php }?>
+		</div>
+<?php
+	/*if(!access()){
+		// Der er ikke logget ind, vis login
+		include "login.php";
+		die();
+	}*/
+?>
