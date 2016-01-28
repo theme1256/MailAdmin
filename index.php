15d9aaa2afdd410380f0b4557340f9f5dddabfa9
Folkmann <theme1256@gmail.com>
2016-01-28 21:46:49 +0100

    Fil som håndterer at vise de forskellige informationer

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	include "top.php";
+
+	if(!empty($d)){
+		if(!empty($m)){
+			// Vis info om den givne mail på det givne domæne
+		}
+		else{
+			// Vis liste med mails der hører til det domæne
+		}
+	}
+	else{
+		// Vis liste med domæner brugeren er admin over
+	}
+
+	include "foot.php";
+?>
