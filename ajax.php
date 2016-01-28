8db2e968b4084ec49364e2b1828f05cfd87d681b
Folkmann <theme1256@gmail.com>
2016-01-29 00:48:14 +0100

    Kan logge ind og opdatere en bruger

--- ajax.php
+++ ajax.php
@@ -44,15 +44,67 @@
 	}
 	elseif($action == "editUser"){
 		// Ret en bruger
+		$u = rens($_POST['u']);
+		$uID = rens($_POST['uID']);
+		$p = rens($_POST['p']);
+		$dd = rens($_POST['dd']);
+		if(empty($u)){
+			echo "Fejl: Brugernavn er tomt.";
+			exit;
+		}
+		else{
+			$sql = "UPDATE login SET u='$u' WHERE uID=$uID";
+			if(!mysqli_query($db, $sql)){
+				mysqli_rollback($db);
+				echo "Fejl: SQL, opdatering af navn. $sql";
+				exit;
+			}
+		}
+		if(!empty($p)){
+			$p = haash($p);
+			$sql = "UPDATE login SET p='$p' WHERE uID=$uID";
+			if(!mysqli_query($db, $sql)){
+				mysqli_rollback($db);
+				echo "Fejl: SQL, opdatering af kode. $sql";
+				exit;
+			}
+		}
+		$sql = "DELETE FROM con WHERE uID=$uID";
+		if(!mysqli_query($db, $sql)){
+			mysqli_rollback($db);
+			echo "Fejl: SQL, sletning af gamle rettighedder. $sql";
+			exit;
+		}
+		$v = "";
+		$V = explode(",", $dd);
+		foreach($V as $k => $val){
+			if($k > 0)
+				$v .= ",";
+			$v .= "('$val','$uID')";
+		}
+		$sql = "INSERT INTO con (dID, uID) VALUES $v";
+		if(!mysqli_query($db, $sql)){
+			mysqli_rollback($db);
+			echo "Fejl: SQL, rettighedder til bruger. $sql";
+			exit;
+		}
+		else{
+			mysqli_commit($db);
+			echo "Succes";
+		}
 	}
 	elseif($action == "login"){
 		// Tjek om de givne informationer stemmer overens med noget i DB
 		$u = rens($_POST['u']);
 		$p = rens($_POST['p']);
+		if(empty($p) || empty($u)){
+			echo "Fejl: Et felt er tomt.";
+			exit;
+		}
 		$q = mysqli_query($db, "SELECT * FROM login WHERE u='$u'");
 		if(mysqli_num_rows($q) > 0){
 			$r = mysqli_fetch_array($q);
-			if(hash_equals(haash($p, $r['p']), $r['p'])){
+			if(haash($p, $r['p']) == $r['p']){
 				// Passer
 				$_SESSION['user'] = $r['uID'];
 				echo "Succes";
