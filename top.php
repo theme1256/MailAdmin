6ef4b033ee0f4b9dd4057efbc764e29ade48e6ce
Folkmann <theme1256@gmail.com>
2016-01-29 00:46:59 +0100

    Lavede funktioner til at sende beskeder og fjerne dem igen og lidt layout

--- top.php
+++ top.php
@@ -8,8 +8,37 @@
 		<meta charset="utf-8">
 		<meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
 		<meta http-equiv="Pragma" content="no-cache"/>
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,700,400italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
-		<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+		<link rel="stylesheet" href="/style.css" type="text/css" media="all" />
+		<script type="text/javascript">
+		var cleaner;
+		var interval
+
+		// Fjerner msg og sætter en timer til at få tømt den
+		function clean(){
+			$(".msg").slideUp(300);
+			cleaner = setInterval("empt()", 2000);
+		}
+
+		// Tømmer msg
+		function empt(){
+			$(".msg").html("");
+			clearInterval(cleaner);
+		}
+
+		// Sætter en besked "m" med classen "v"
+		function setmsg(m, v){
+			$(".msg").html("<div class=\""+v+"\">"+m+"</div>");
+			$(".msg").slideDown(300);
+			cleaner = setInterval("clean()", 10000);
+		}
+
+		function ReLoad(){
+			location.reload();
+			clearInterval(interval);
+		}
+		</script>
 	</head>
 	<body>
 		<div class="header">
@@ -18,6 +47,8 @@
 			<span>Domæne: <?php echo $d;?></span>
 			<?php }?>
 		</div>
+		<div class="content">
+			<div class="msg"></div>
 <?php
 	if(!access()){
 		// Der er ikke logget ind, vis login
