061634f27bf5562f8a26fbac759b70c75f409d8d
Folkmann <theme1256@gmail.com>
2016-01-29 00:48:39 +0100

    Viser beskeder, hvis der sker fejl og reloader hvis korrekt

--- login.php
+++ login.php
@@ -1,16 +1,26 @@
 <h2>Du har måske ikke adgang her, log lige ind</h2>
-<form id="login">
+<form class="login">
 	Brugernavn:<br/>
 	<input type="text" name="u" placeholder="username"/><br/>
 	Kodeord:<br/>
 	<input type="password" name="p" placeholder="password"/><br/>
-	<input type="submit" value="Login"/>
+	<button class="submit">Login</button>
 </form>
 <script type="text/javascript">
 	$(function(){
-		$("form").submit(function(e){
+		$(".submit").click(function(e){
 			e.preventDefault();
-			console.log("klikket");
+			var U = $("input[name='u']").val();
+			var P = $("input[name='p']").val();
+			$.post("/ajax.php", {action: "login", u: U, p: P}).done(function(r){
+				if(r == "Succes"){
+					setmsg("Login lykkedes, reloader siden.", "succes");
+					interval = setInterval(ReLoad(), 2500);
+				}
+				else{
+					setmsg(r, "error");
+				}
+			});
 		});
 	});
 </script>
