007da63730efdc9673428a48ee0550ed13ac3064
Folkmann <theme1256@gmail.com>
2016-01-30 23:14:28 +0100

    AJAX er så godt som færdig, mangler kun at kunne oprette mapperne

--- ajax.php
+++ ajax.php
@@ -16,10 +16,9 @@
 		}
 		// Opret brugeren i DB
 		$p = haash($p);
-		$sql = "INSERT INTO login (u, p) VALUES ('$u','$p')";
-		if(!mysqli_query($db, $sql)){
+		if(!mysqli_query($db, "INSERT INTO login (u, p) VALUES ('$u','$p')")){
 			mysqli_rollback($db);
-			echo "Fejl: SQL, indtastning af bruger. $sql";
+			echo "Fejl: SQL, indtastning af bruger.";
 			exit;
 		}
 		$id = mysqli_insert_id($db);
@@ -31,10 +30,9 @@
 				$v .= ",";
 			$v .= "('$val','$id')";
 		}
-		$sql = "INSERT INTO con (dID, uID) VALUES $v";
-		if(!mysqli_query($db, $sql)){
+		if(!mysqli_query($db, "INSERT INTO con (dID, uID) VALUES $v")){
 			mysqli_rollback($db);
-			echo "Fejl: SQL, rettighedder til bruger. $sql";
+			echo "Fejl: SQL, rettighedder til bruger.";
 			exit;
 		}
 		else{
@@ -53,26 +51,23 @@
 			exit;
 		}
 		else{
-			$sql = "UPDATE login SET u='$u' WHERE uID=$uID";
-			if(!mysqli_query($db, $sql)){
+			if(!mysqli_query($db, "UPDATE login SET u='$u' WHERE uID=$uID")){
 				mysqli_rollback($db);
-				echo "Fejl: SQL, opdatering af navn. $sql";
+				echo "Fejl: SQL, opdatering af navn.";
 				exit;
 			}
 		}
 		if(!empty($p)){
 			$p = haash($p);
-			$sql = "UPDATE login SET p='$p' WHERE uID=$uID";
-			if(!mysqli_query($db, $sql)){
+			if(!mysqli_query($db, "UPDATE login SET p='$p' WHERE uID=$uID")){
 				mysqli_rollback($db);
-				echo "Fejl: SQL, opdatering af kode. $sql";
+				echo "Fejl: SQL, opdatering af kode.";
 				exit;
 			}
 		}
-		$sql = "DELETE FROM con WHERE uID=$uID";
-		if(!mysqli_query($db, $sql)){
+		if(!mysqli_query($db, "DELETE FROM con WHERE uID=$uID")){
 			mysqli_rollback($db);
-			echo "Fejl: SQL, sletning af gamle rettighedder. $sql";
+			echo "Fejl: SQL, sletning af gamle rettighedder.";
 			exit;
 		}
 		$v = "";
@@ -82,10 +77,9 @@
 				$v .= ",";
 			$v .= "('$val','$uID')";
 		}
-		$sql = "INSERT INTO con (dID, uID) VALUES $v";
-		if(!mysqli_query($db, $sql)){
+		if(!mysqli_query($db, "INSERT INTO con (dID, uID) VALUES $v")){
 			mysqli_rollback($db);
-			echo "Fejl: SQL, rettighedder til bruger. $sql";
+			echo "Fejl: SQL, rettighedder til bruger.";
 			exit;
 		}
 		else{
@@ -103,19 +97,17 @@
 			exit;
 		}
 		else{
-			$sql = "UPDATE login SET u='$u' WHERE uID=$uID";
-			if(!mysqli_query($db, $sql)){
+			if(!mysqli_query($db, "UPDATE login SET u='$u' WHERE uID=$uID")){
 				mysqli_rollback($db);
-				echo "Fejl: SQL, opdatering af navn. $sql";
+				echo "Fejl: SQL, opdatering af navn.";
 				exit;
 			}
 		}
 		if(!empty($p)){
 			$p = haash($p);
-			$sql = "UPDATE login SET p='$p' WHERE uID=$uID";
-			if(!mysqli_query($db, $sql)){
+			if(!mysqli_query($db, "UPDATE login SET p='$p' WHERE uID=$uID")){
 				mysqli_rollback($db);
-				echo "Fejl: SQL, opdatering af kode. $sql";
+				echo "Fejl: SQL, opdatering af kode.";
 				exit;
 			}
 		}
@@ -158,10 +150,9 @@
 			exit;
 		}
 		// Opret domæne i DB
-		$sql = "INSERT INTO domains (domain) VALUES ('$d')";
-		if(!mysqli_query($db, $sql)){
+		if(!mysqli_query($db, "INSERT INTO domains (domain) VALUES ('$d')")){
 			mysqli_rollback($db);
-			echo "Fejl: SQL, indtastning af domæne. $sql";
+			echo "Fejl: SQL, indtastning af domæne.";
 			exit;
 		}
 		$id = mysqli_insert_id($db);
@@ -173,10 +164,9 @@
 				$v .= ",";
 			$v .= "('$id','$val')";
 		}
-		$sql = "INSERT INTO con (dID, uID) VALUES $v";
-		if(!mysqli_query($db, $sql)){
+		if(!mysqli_query($db, "INSERT INTO con (dID, uID) VALUES $v")){
 			mysqli_rollback($db);
-			echo "Fejl: SQL, rettighedder til bruger. $sql";
+			echo "Fejl: SQL, rettighedder til bruger.";
 			exit;
 		}
 		else{
@@ -186,9 +176,160 @@
 	}
 	elseif($action == "newMail"){
 		// Opret ny mail eller liste, tjek først om ting er tomme, derefter om det er en mail eller en liste
+		$t = rens($_POST['t']); // "mail" eller "list"
+		$b = rens($_POST['b']); // Antallet af modtagere
+		$bb = rens($_POST['bb']); // Listen med modtagere, hvis det er en liste
+		$m = rens($_POST['m']); // Mailadressen minus domænet
+		$d = rens($_POST['d']); // Domænet
+		$p = rens($_POST['p']); // Kodeordet der skal bruges hvis det er en lokal mail
+		// Tjek tomme felter
+		if($t == "mail"){
+			if(empty($p)){
+				echo "Fejl: Der er ikke skrevet et kodeord.";
+				exit;
+			}
+			if(empty($m)){
+				echo "Fejl: Der er ikke skrevet noget før @$d.";
+				exit;
+			}
+		}
+		else{
+			if(empty($bb) || $bb == "undefined"){
+				echo "Fejl: Der er ikke nogne modtagere.";
+				exit;
+			}
+		}
+		// Tjek om adressen allerede findes
+		if(mysqli_num_rows(mysqli_query($db, "SELECT * FROM aliases WHERE mail='$m@$d'")) > 0){
+			echo "Fejl: Den mail er allerede i databasen.";
+			exit;
+		}
+		if($t == "mail"){
+			// Opret som lokal mail, husk at oprette mapper det rigtige sted
+			// Mappe: /var/spool/mail/virtual/[domæne]/[mail]/
+			// Opret alias
+			if(!mysqli_query($db, "INSERT INTO aliases (mail, destination) VALUES ('$m@$d','$m@$d')")){
+				mysqli_rollback($db);
+				echo "Fejl: SQL, oprettelse af internt alias. $sql";
+				exit;
+			}
+			// Oprettelse af bruger
+			if(!mysqli_query($db, "INSERT INTO users (id, name, maildir, crypt) VALUES ('$m@$d','$m','$d/$m/',encrypt('$p', CONCAT('$5$', MD5(RAND()))))")){
+				mysqli_rollback($db);
+				echo "Fejl: SQL, oprettelse af internt alias. $sql";
+				exit;
+			}
+			// Opret mappe til domæne, hvis den ikke findes
+
+			// Opret mappe til bruger, hvis den ikke findes
+			
+		}
+		else{
+			// Opret som liste
+			if(!mysqli_query($db, "INSERT INTO aliases (mail, destination) VALUES ('$m@$d','$bb')")){
+				mysqli_rollback($db);
+				echo "Fejl: SQL, oprettelse af liste. $sql";
+				exit;
+			}
+			else{
+				mysqli_commit($db);
+				echo "Succes";
+			}
+		}
 	}
 	elseif($action == "editMail"){
 		// Ret den givne mail eller liste, slet login, hvis det er skift til liste, opret login hvis skift til mail
+		$t = rens($_POST['t']); // "mail" eller "list"
+		$b = rens($_POST['b']); // Antallet af modtagere
+		$bb = rens($_POST['bb']); // Listen med modtagere, hvis det er en liste
+		$m = rens($_POST['m']); // Mailadressen minus domænet
+		$d = rens($_POST['d']); // Domænet
+		$p = rens($_POST['p']); // Kodeordet der skal bruges hvis det er en lokal mail
+		$aID = rens($_POST['aID']); // pkid til alias, hvis det var en liste
+		$u = rens($_POST['u']); // Den gamle mail, hvis det var en lokal mail
+		// Tjek tomme felter
+		if($t == "mail"){
+			if(empty($p)){
+				echo "Fejl: Der er ikke skrevet et kodeord.";
+				exit;
+			}
+			if(empty($m)){
+				echo "Fejl: Der er ikke skrevet noget før @$d.";
+				exit;
+			}
+		}
+		else{
+			if(empty($bb) || $bb == "undefined"){
+				echo "Fejl: Der er ikke nogne modtagere.";
+				exit;
+			}
+		}
+		// Opdater DB
+		if($u == "" && $t == "list"){
+			// Var en liste, skal stadig være det
+			// Opdater pkid til det nye
+			if(!mysqli_query($db, "UPDATE aliases SET mail='$m@$d', destination='$bb' WHERE pkid=$aID")){
+				mysqli_rollback($db);
+				echo "Fejl: SQL, opdatering af liste.";
+				exit;
+			}
+			else{
+				mysqli_commit($db);
+				echo "Succes";
+			}
+		}
+		elseif($u == "" && $t == "mail"){
+			// Var en liste, skal være en lokal mail
+			// Ret alias
+			if(!mysqli_query($db, "UPDATE aliases SET mail='$m@$d', destination='$m@$d' WHERE pkid=$aID")){
+				mysqli_rollback($db);
+				echo "Fejl i opdatering af alias.";
+				exit;
+			}
+			// Opret bruger
+			if(!mysqli_query($db, "INSERT INTO users (id, name, maildir, crypt) VALUES ('$m@$d','$m','$d/$m/',encrypt('$p', CONCAT('$5$', MD5(RAND()))))")){
+				mysqli_rollback($db);
+				echo "Fejl i oprettelse af bruger.";
+				exit;
+			}
+			// Opret mappe til domæne, hvis den ikke findes
+
+			// Opret mappe til bruger, hvis den ikke findes
+
+		}
+		elseif(strlen($u) > 0 && $t == "list"){
+			// Var en mail, skal være en liste
+			// Fjern fra users
+			if(!mysqli_query($db, "DELETE FROM users WHERE id='$m@$d'")){
+				mysqli_rollback($db);
+				echo "Fejl i opdatering af kodeord.";
+				exit;
+			}
+			// Opdater alias
+			if(!mysqli_query($db, "UPDATE aliases SET mail='$m@$d', destination='$bb' WHERE pkid=$aID")){
+				mysqli_rollback($db);
+				echo "Fejl i opdatering af alias.";
+				exit;
+			}
+			else{
+				mysqli_commit($db);
+				echo "Succes";
+			}
+		}
+		else{
+			// Går ud fra at det var en mail og stadig skal være en
+			// Beholder den gamle mappe, da der ikke ændres mail-navn
+			// Opdaterer kode
+			if(!mysqli_query($db, "UPDATE users SET crypt=encrypt('$p', CONCAT('$5$', MD5(RAND()))) WHERE id='$m@$d'")){
+				mysqli_rollback($db);
+				echo "Fejl i opdatering af kodeord.";
+				exit;
+			}
+			else{
+				mysqli_commit($db);
+				echo "Succes";
+			}
+		}
 	}
 	else{
 		// Action er ikke sat, giv fejl
