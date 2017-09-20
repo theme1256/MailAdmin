0ab9c393a8f12d45253b5709898f26295a87007a
Folkmann <theme1256@gmail.com>
2017-09-20 17:55:11 +0200

    Slettede den gamle kode

--- _OLD/foot.php
+++
@@ -1,8 +0,0 @@
-		</div>
-		<footer class="footer">
-			<div class="container">
-				<p class="text-muted text-center">&copy; Folkmann IT</p>
-			</div>
-		</footer>
-	</body>
-</html>
--- _OLD/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include "common.php";
-
-	session_destroy();
-
-	header("Location: /");
-?>
